2cc45b705eec9f71a6e0ca762fdc03a117ea4ae2
glennswest <gwest@redhat.com>
2019-02-28 14:16:03 -0600

    Add tmp

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 export GIT_COMMIT=$(git rev-parse --short HEAD)
+rm -r -f tmp
 mkdir tmp
-echo $GIT_COMMIT > tmp/commit.id
-chmod -r 777 tmp
+echo $GIT_COMMIT > ./tmp/commit.id
+chmod -R 777 ./tmp
 #eval $(minishift docker-env)
 docker build --no-cache -t glennswest/winoperator:$GIT_COMMIT .
 docker tag glennswest/winoperator:$GIT_COMMIT  docker.io/glennswest/winoperator:$GIT_COMMIT
