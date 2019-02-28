ca8707a5bdf044e9d4b6d47b2b4eebedbfea3321
“gwest“ <gwest@redhat.com>
2019-02-28 11:33:47 -0600

    Add cluster role to operator

--- run.sh
+++ run.sh
@@ -7,9 +7,11 @@ oc delete sa winoperator
 oc delete project winoperator
 sleep 3
 oc new-project winoperator
-#oc create sa winoperator
-#oc policy add-role-to-user admin  system:serviceaccount:winoperator:winoperator
+oc create sa winoperator
+oc policy add-role-to-user admin  system:serviceaccount:winoperator:winoperator
+oc policy add-role-to-user cluster-admin system:serviceaccount:winoperator:winoperator
 oc import-image winoperator --from=docker.io/glennswest/winoperator:$GIT_COMMIT --confirm
-#oc new-app glennswest/winoperator:$GIT_COMMIT --token=$(oc sa get-token winoperator) 
-oc new-app docker.io/glennswest/winoperator:$GIT_COMMIT
+oc delete  istag/winoperator:latest
+oc new-app glennswest/winoperator:$GIT_COMMIT --token=$(oc sa get-token winoperator) 
+#oc new-app --docker-image="docker.io/glennswest/winoperator:$GIT_COMMIT"
 
