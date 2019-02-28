9851279ad630ec2b75a17af81b73e6cb1464c4af
“gwest“ <gwest@redhat.com>
2019-02-28 10:25:46 -0600

    Fixup run

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
 export GIT_COMMIT=$(git rev-parse --short HEAD)
+echo $GIT_COMMIT
+docker pull docker.io/glennswest/winoperator:$GIT_COMMIT
 oc delete dc winoperator
 oc delete is winoperator
 oc delete rc winoperator
 oc delete sa winoperator
+oc delete project winoperator
 sleep 3
-#oc create sa winoperator
-oc create clusterrolebinding default-view --clusterrole=view --serviceaccount=default:default
-#oc adm policy add-role-to-user admin system:serviceaccount:winoperator 
-oc new-app docker.io/glennswest/winoperator:$GIT_COMMIT
+oc new-project winoperator
+oc create sa winoperator
+oc policy add-role-to-user admin  system:serviceaccount:winoperator:winoperator
+oc new-app glennswest/winoperator:$GIT_COMMIT --token=$(oc sa get-token winoperator) 
