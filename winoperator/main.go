e57e0f385abda6b616be69efc38fa042593e6919
glennswest <gwest@redhat.com>
2019-02-28 16:20:09 -0600

    Fix up log

--- winoperator/main.go
+++ winoperator/main.go
@@ -14,15 +14,15 @@ import (
 
 func init() {
   // Log as JSON instead of the default ASCII formatter.
-  //log.SetFormatter(&log.JSONFormatter{})
-  log.SetFormatter(&log.TextFormatter{})
+  log.SetFormatter(&log.JSONFormatter{})
+  //log.SetFormatter(&log.TextFormatter{})
 
   // Output to stdout instead of the default stderr
   // Can be any io.Writer, see below for File example
-  log.SetOutput(os.Stdout)
+  //log.SetOutput(os.Stdout)
 
   // Only log the warning severity or above.
-  log.SetLevel(log.TraceLevel)
+  //log.SetLevel(log.TraceLevel)
 }
 
 func main() {
