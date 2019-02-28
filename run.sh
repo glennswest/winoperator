dc03ebcd330ce8c90f1a97d61105dfd5d273a144
“gwest“ <gwest@redhat.com>
2019-02-28 10:57:32 -0600

    Temp disable sa

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 export GIT_COMMIT=$(git rev-parse --short HEAD)
 echo $GIT_COMMIT
-docker pull docker.io/glennswest/winoperator:$GIT_COMMIT
 oc delete dc winoperator
 oc delete is winoperator
 oc delete rc winoperator
@@ -8,6 +7,9 @@ oc delete sa winoperator
 oc delete project winoperator
 sleep 3
 oc new-project winoperator
-oc create sa winoperator
-oc policy add-role-to-user admin  system:serviceaccount:winoperator:winoperator
-oc new-app glennswest/winoperator:$GIT_COMMIT --token=$(oc sa get-token winoperator) 
+#oc create sa winoperator
+#oc policy add-role-to-user admin  system:serviceaccount:winoperator:winoperator
+oc import-image winoperator --from=docker.io/glennswest/winoperator:$GIT_COMMIT --confirm
+#oc new-app glennswest/winoperator:$GIT_COMMIT --token=$(oc sa get-token winoperator) 
+oc new-app docker.io/glennswest/winoperator:$GIT_COMMIT
+
