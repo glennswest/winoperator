f2c3d04f586657776acac478aa6f514095094f3e
glennswest <gwest@redhat.com>
2019-02-28 08:55:52 -0600

    Fix build to use git commit properly

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 export GIT_COMMIT=$(git rev-parse --short HEAD)
 #eval $(minishift docker-env)
-docker build --no-cache -t glennswest/winoperator .
-docker tag glennswest/winoperator:latest docker.io/glennswest/winoperator:$GIT_COMMIT
+docker build --no-cache -t glennswest/winoperator:$GIT_COMMIT .
+docker tag glennswest/winoperator:$GIT_COMMIT  docker.io/glennswest/winoperator:$GIT_COMMIT
 docker push docker.io/glennswest/winoperator:$GIT_COMMIT
