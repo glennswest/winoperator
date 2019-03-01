8751a1643eb175b4702af4acae20086e2261e092
“gwest“ <gwest@redhat.com>
2019-03-01 10:53:47 -0600

    Container running with rbac correct

--- run.sh
+++ run.sh
@@ -2,16 +2,14 @@ export GIT_COMMIT=$(git rev-parse --short HEAD)
 echo $GIT_COMMIT
 oc delete dc winoperator
 oc delete is winoperator
-oc delete rc winoperator
 oc delete sa winoperator
 oc delete project winoperator
 sleep 3
 oc new-project winoperator
 oc create sa winoperator
-oc policy add-role-to-user admin  system:serviceaccount:winoperator:winoperator
-oc policy add-role-to-user cluster-admin system:serviceaccount:winoperator:winoperator
+oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:winoperator:default
+oc policy add-role-to-user admin  system:serviceaccount:winoperator:default
 oc import-image winoperator --from=docker.io/glennswest/winoperator:$GIT_COMMIT --confirm
 oc delete  istag/winoperator:latest
 oc new-app glennswest/winoperator:$GIT_COMMIT --token=$(oc sa get-token winoperator) 
-#oc new-app --docker-image="docker.io/glennswest/winoperator:$GIT_COMMIT"
 
