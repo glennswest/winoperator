ffad0d7cee40b3624e65dd493170b443b2d0729d
“gwest“ <gwest@redhat.com>
2019-05-02 09:04:54 -0500

    add volume

--- run.sh
+++ run.sh
@@ -7,10 +7,10 @@ oc delete sa winoperator
 oc delete project winoperator
 sleep 3
 oc new-project winoperator
-oc set volume dc/winoperator --add --name=dbdata --type=hostPath --path=/etc/winoperator --mount-path=/data
 oc import-image winoperator --from=docker.io/glennswest/winoperator:$GIT_COMMIT --confirm
 oc delete  istag/winoperator:latest
 #oc create sa winoperator
+oc set volume dc/winoperator --add --name=dbdata --type=hostPath --path=/etc/winoperator --mount-path=/data
 oc new-app glennswest/winoperator:$GIT_COMMIT --token=$(oc sa get-token winoperator) 
 oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:winoperator:default
 oc policy add-role-to-user admin  system:serviceaccount:winoperator:default
