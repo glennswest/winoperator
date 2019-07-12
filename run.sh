30fadbf586e48e32c75f39cc6532f4dbc9c84da2
gwest <gwest@redhat.com>
2019-07-13 01:09:49 +0800

    fix up run.sh to enable it to work on ocp4.2 for winoperator

--- run.sh
+++ run.sh
@@ -9,19 +9,19 @@ sleep 40
 oc new-project winoperator
 #oc import-image winoperator --from=docker.io/glennswest/winoperator:$GIT_COMMIT --confirm
 #oc delete  istag/winoperator:latest
-#oc create sa winoperator
+oc create sa winoperator
+oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:winoperator:default
 kubectl create deployment winoperator --image=docker.io/glennswest/winoperator:$GIT_COMMIT
 #oc set volume dc/winoperator --add --name=dbdata --type=hostPath --path=/etc/winoperator --mount-path=/data
 #oc new-app --docker-image=glennswest/winoperator:$GIT_COMMIT --token=$(oc sa get-token winoperator) 
 #oc run winoperator --tty --stdin --image=glennswest/winoperator:$GIT_COMMIT
-#oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:winoperator:default
 #oc policy add-role-to-user admin  system:serviceaccount:winoperator:default
 sleep 20
 export masterhostname=control-plane-0
 export sshkey=`cat ~/.ssh/id_rsa | base64 | tr -d '\n'`
 export workerign=`cat worker.ign | base64 | tr -d '\n'`
 export defaultdomain=$(oc describe --namespace=openshift-ingress-operator ingresscontroller/default | grep "Domain:" | cut -d ":" -f 2 | cut -d "." -f 1- | tr -d ' ')
-oc set env deployment/winoperator WINMACHINEMAN=winmachineman.$defaultdomain
+oc set env deployment/winoperator WINMACHINEMAN=http://winmachineman.$defaultdomain
 oc set env deployment/winoperator SSHKEY=$sshkey
 oc set env deployment/winoperator MASTERHOST=$masterhostname
 oc set env deployment/winoperator WORKERIGN=$workerign
